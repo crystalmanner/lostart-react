5c333c8b6b33c56d99b0446fddffdef3d093b9f8
crystalmanner <crystalmanner130@outlook.com>
2019-06-08 05:34:39 +0900

    change contentfulCMS account

--- config/contentful.php
+++ config/contentful.php
@@ -11,8 +11,8 @@ return [
     /*
      * The ID of the space you want to access.
      */
-    'space' => env('CONTENTFUL_SPACE_ID','f9qtzu9dvrki'),
-    
+    // 'space' => env('CONTENTFUL_SPACE_ID','f9qtzu9dvrki'),
+    'space' => env('CONTENTFUL_SPACE_ID','ldgy0e55mhnf'),
     /*
      * The ID of the environment you want to access.
      */
@@ -21,8 +21,8 @@ return [
     /*
      * An API key for the above specified space.
      */
-    'token' => env('CONTENTFUL_DELIVERY_TOKEN','bef8878636daf1caabe2f86fea898a7b0c1383e4e20931164eac2fafd6e6d13a'),
-
+    // 'token' => env('CONTENTFUL_DELIVERY_TOKEN','bef8878636daf1caabe2f86fea898a7b0c1383e4e20931164eac2fafd6e6d13a'),
+    'token' => env('CONTENTFUL_DELIVERY_TOKEN','1c4e6709e2d0b240f07839298e91e479a511d6deaaf89fff8bce9f1b7b0b0b76'),
     /*
      * Controls whether Contentful's Delivery or Preview API is accessed.
      */
