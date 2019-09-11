73980aaa4f20570f4a54b54fdcf8c6c83422dc7b
crystalmanner <crystalmanner130@outlook.com>
2019-09-11 10:38:21 +0900

    revert

--- src/html.js
+++ src/html.js
@@ -42,7 +42,7 @@ export default function HTML(props) {
             `
           }}
         /> */}
-        {/* 
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -53,17 +53,17 @@ export default function HTML(props) {
             })(window,document,'script','dataLayer','GTM-KH3W5JP');
             `
           }}
-        /> */}
-        {/* <script src="https://use.typekit.net/sbj4klj.js"></script>
+        />
+        <script src="https://use.typekit.net/sbj4klj.js"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
                 try{Typekit.load({ async: true });}catch(e){}
             `
           }}
-        /> */}
+        />
 
-        {/* <script async rc="https://www.googletagmanager.com/gtag/js?id=UA-140149190-1"></script> */}
+        <script async rc="https://www.googletagmanager.com/gtag/js?id=UA-140149190-1"></script>
         {/* <script
             dangerouslySetInnerHTML={{
             __html: `
@@ -75,7 +75,7 @@ export default function HTML(props) {
             `
             }}
         /> */}
-        {/* <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
@@ -84,9 +84,9 @@ export default function HTML(props) {
 
             gtag('config', 'UA-140149190-1');
             `
-          }} */}
+          }}
         />
-        {/* <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5882190.js"></script> */}
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/5882190.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {/* <noscript
@@ -95,11 +95,11 @@ export default function HTML(props) {
             height="0" width="0" style="display:none;visibility:hidden">
             </iframe>`
           }} /> */}
-        {/* <noscript
+        <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KH3W5JP"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-          }} /> */}
+          }} />
         {props.preBodyComponents}
         <noscript key="noscript" id="gatsby-noscript">
           This app works best with JavaScript enabled.
